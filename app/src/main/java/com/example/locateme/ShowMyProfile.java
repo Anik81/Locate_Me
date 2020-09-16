4c7b9ad6e535d557747c458705efcf07fd7fa52a
Tanvir Rahman <anik81.art@gmail.com>
2020-09-17 02:04:33 +0600

    Setting,Contact,Edit profile layout changed

---
+++ app/src/main/java/com/example/locateme/ShowMyProfile.java
@@ -0,0 +1,14 @@
+package com.example.locateme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowMyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_my_profile);
+    }
+}
